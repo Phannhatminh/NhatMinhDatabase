ba252297cdbaa498bec5feadfdff72261e15a3ac
Phannhatminh <118093443+Phannhatminh@users.noreply.github.com>
2023-06-05 00:21:45 +0700

    modify main

--- main.cpp
+++ main.cpp
@@ -58,37 +58,7 @@ class Application {
         Application& operator=(const Application&) = delete;
 };
 
-
-
-
 int main(int argc, char **argv) {
-    //    /usr/bin/clang++ -std=c++17 -pthread -fcolor-diagnostics -fansi-escape-codes -g /Users/nhatminh/NhatMinhDatabase/main.cpp -o /Users/nhatminh/NhatMinhDatabase/main
-    int i = 0;
-    StorageEngine Engine;
-    Engine.getInstance().createDatabase("MySchool74");
-    Database db = Engine.getInstance().openDatabase("MySchool74");
-    ColumnDef column_1("id", DBType :: INT, sizeof(int));
-    ColumnDef column_2("name", DBType :: STRING, 30);
-    ColumnDefs colDefs;
-    colDefs.addColumn(column_1);
-    colDefs.addColumn(column_2);
-    DBValue val_1(1);
-    DBValue val_2("Phan Nhat Minh");
-    Row row(colDefs);
-    DBValue val_3(2);
-    DBValue val_4("Pham Duy Long");
-    Row row_1(colDefs);
-    row_1.setColumnDefs(colDefs);
-    row_1.setValueByColumnName("id", val_3);
-    row_1.setValueByColumnName("name", val_4);
-    row.setColumnDefs(colDefs);
-    row.setValueByColumnName("id", val_1);
-    row.setValueByColumnName("name", val_2);
-    db.createTable("hocsinh", colDefs);
-    UserTable tbl = db.getTableByName("hocsinh");
-    tbl.insertRow(row);
-    tbl.insertRow(row_1);
-
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
